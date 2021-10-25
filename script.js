973a2bd1058f992e758192aaff7db4be1300ec4c
Sidharth Sethi <Sidharth.sherry@gmail.com>
2021-10-26 00:14:54 +0530

    fixed Typo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const links = document.getElementById("links");
 for (let i = 1; i <= 50; i++) {
   const link = document.createElement("a");
   link.classList.add("link");
-  link.href = "day" + i + "/index.html";
+  link.href = "Day" + i + "/index.html";
   link.target = "_blank";
   link.innerText = "Day " + i;
   links.appendChild(link);
